80d5768a4f9385f346b74e6ad238ead3140cc322
UN5 Coin <jackmoymlw@gmail.com>
2025-07-06 21:45:28 +0800

    Create data.js

---
+++ data.js
@@ -0,0 +1,24 @@
+const data = [
+  {
+    name: "UN5 LP NFT #1",
+    tokenId: "81260",
+    value: "$12,100",
+    mage: "https://via.placeholder.com/300x300?text=UN5+NFT+1",
+  opensea: "https://opensea.io/assets/ethereum/0xc36442b4a4522e871399cd717abdd847ab11fe88/81260"
+
+  },
+  {
+    name: "Uniswap - 0.3% - UN5/WBTC - 99.760<->MAX",
+    tokenId: "30724",
+    value: "$12,100",
+    image: https://via.placeholder.com/300x300?text=UN5/WBTC
+    opensea: https://opensea.io/assets/ethereum/0xbd2c16513d748c81c4c747f26e3a6a6420f6de9e/30724
+  },
+  {
+    name: "Uniswap - 0.3% - UN5/UNI - MIN<->MAX",
+    tokenId: "30729",
+    value: "$8,300",
+    image: https://via.placeholder.com/300x300?text=UN5/UNI
+    opensea: https://opensea.io/assets/ethereum/0xbd2c16513d748c81c4c747f26e3a6a6420f6de9e/30729
+  }
+];
