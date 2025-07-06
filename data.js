b0e293bd787655f00f9838c2efe9a79fc18b0e79
UN5 Coin <jackmoymlw@gmail.com>
2025-07-06 22:08:10 +0800

    Update data.js

--- data.js
+++ data.js
@@ -21,4 +21,11 @@ const data = [
     image: https://via.placeholder.com/300x300?text=UN5/UNI
     opensea: https://opensea.io/assets/ethereum/0xbd2c16513d748c81c4c747f26e3a6a6420f6de9e/30729
   }
-];
+  ];
+  {
+    name: "Uniswap - 0.3% - UN5/WETH - 831780<>11108000",
+    tokenId: "1025988",
+    value: "$3,800",
+    image: "https://via.placeholder.com/300x300?text=UN5/WETH",
+    opensea: "https://opensea.io/assets/ethereum/0xc36442b4a4522e871399cd717abd8b41f8e88d/1025988"
+  }
